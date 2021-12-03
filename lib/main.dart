21d70c87e61bbc2740368711dc3c1ae1dc235589
Erick Vinicius <ericktjdev@gmail.com>
2021-12-03 11:14:03 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:ishowapp/Login.dart';
+
+void main() => runApp(
+  MaterialApp(
+    home: Login(),
+    debugShowCheckedModeBanner: false,
+  )
+);
+
